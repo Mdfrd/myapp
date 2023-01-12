48f865acb083df8b01398d495e39db2f287b73b7
Mdfrd <0muhammadfarid@gmail.com>
2023-01-12 19:26:05 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const port1 = 8000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send(`You're at your profile page`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening
+at http://localhost:${port}`)
+})
+
+app.listen(port1, () => {
+    console.log(`Example app listening
+at http://localhost:${port1}`)
+})
